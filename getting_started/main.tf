9b21babd03e007e5d7daeecc5d1a33b170feee51
Gourav Anandache <g.anandache@sap.com>
2022-03-10 15:19:47 +0530

    finished chapter 1

--- getting_started/main.tf
+++ getting_started/main.tf
@@ -1,3 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.58.0"
+      # configuration_aliases = [aws.alternate]
+    }
+  }
+
+  backend "remote" {
+    organization = "rnd174"
+    workspaces {
+      name = "getting-started"
+    }
+  }
+
+  #    cloud {
+  #     organization = "rnd174"
+  #     workspaces {
+  #       name = "getting-started"
+  #     }
+  #   }
+
+}
+
+
 locals {
   project_name = "Gourav"
 }
